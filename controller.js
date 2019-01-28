820f6eca8a99faa53655eff641a4599697c58347
Trevor Sabo <sabotrev@gmail.com>
2019-01-27 21:58:24 -0500

    Added controller.js, Added a sidebar but still needs cleaning up

---
+++ controller.js
@@ -0,0 +1,10 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // TODO: Change to swap a class with different width
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
