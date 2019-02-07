4401d3ea9ebb3420b2ffcaddb25c2e80dfdae3a1
Trevor Sabo <sabotrev@gmail.com>
2019-02-06 20:43:29 -0500

    Another touch of the top & side navigation

--- controller.js
+++ controller.js
@@ -1,11 +1,14 @@
 toggleNav = (element) => {
   var sideNav = document.getElementById("mySidenav");
   var navIcon = document.getElementById("navIcon");
+  var symbol = document.getElementById("symbol");
 
   if (navIcon.className === 'menu') {
-    sideNav.style.width = "250px";
+    sideNav.style.width = "100%";
+    symbol.classList = "fa fa-close";
   } else {
     sideNav.style.width = "0";
+    symbol.classList = "fa fa-bars";
   }
 
   navIcon.classList.toggle('scale-out-center');
