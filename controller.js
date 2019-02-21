332c9a9a7cc5c6a0f25c99ab3975f44b77539b5d
Trevor Sabo <trevor.sabo@gm.com>
2019-02-20 22:57:41 -0500

    Changed color scheme and add about me text

--- controller.js
+++ controller.js
@@ -1,14 +1,17 @@
 toggleNav = (element) => {
-  var sideNav = document.getElementById("mySidenav");
-  var navIcon = document.getElementById("navIcon");
-  var symbol = document.getElementById("symbol");
+  var sideNav = document.getElementById('mySidenav');
+  var navIcon = document.getElementById('navIcon');
+  var symbol = document.getElementById('symbol');
+  var body = document.getElementsByTagName('BODY')[0];
 
   if (navIcon.className === 'menu') {
-    sideNav.style.width = "100%";
-    symbol.classList = "fa fa-close";
+    sideNav.style.width = '100%';
+    symbol.classList = 'fa fa-close';
+    body.style.overflow = 'hidden';
   } else {
-    sideNav.style.width = "0";
-    symbol.classList = "fa fa-bars";
+    sideNav.style.width = '0';
+    symbol.classList = 'fa fa-bars';
+    body.style.overflow = 'unset';
   }
 
   navIcon.classList.toggle('scale-out-center');
